fab1ada78535bd095fa881861ad1e2ead18c1dc8
bender <kudin.dima@gmail.com>
2026-07-02 01:19:53 +0000

    Update src/pages/QuoteRequestPage.tsx

--- src/pages/QuoteRequestPage.tsx
+++ src/pages/QuoteRequestPage.tsx
@@ -10,7 +10,7 @@ export default function QuoteRequestPage() {
         inputs={[{"name":"name","type":"text","placeholder":"Full Name","required":true},{"name":"email","type":"email","placeholder":"Email Address","required":true},{"name":"phone","type":"tel","placeholder":"Phone Number","required":true}]}
         textarea={{"name":"description","placeholder":"Describe the landscaping work required...","rows":5,"required":true}}
         buttonText="Request Quote"
-        imageSrc="https://storage.googleapis.com/webild/users/user_3Fv83KUYlJoI2yCApMcJ43LguDH/uploaded-1782955139186-qvg6mrnt.jpg"
+        imageSrc="https://storage.googleapis.com/webild/users/user_3Fv83KUYlJoI2yCApMcJ43LguDH/uploaded-1782955171786-2nce6d9z.jpg"
         textAnimation="slide-up"
       /></div>
     </>
